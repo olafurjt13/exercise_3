dda9d7043a88d21823f8db059a1e2343f2fe447e
olafurjt13 <olafutjt13@ru.is>
2015-10-10 22:35:28 +0000

    The function is ready I think, until the next bug appears

--- myBackprop.py
+++ myBackprop.py
@@ -2,7 +2,7 @@
 #
 # This program calculates the gradient of the cross-entropy
 # error function for a two layer nerual network given an 
-# input pattern x and weight matrices W1 and W2
+# input pattern x, target vector t and weight matrices W1 and W2
 # 
 # It is assumed that there is only one output variable y
 #
@@ -27,25 +27,24 @@ def networkDeltas(deltaks,ays,W):
 	a = np.array([sigmoid(value) for value in ays])
 	W = np.delete(W,0)
 	deltajs = [sigmoid(a[i]) * (1 - sigmoid(a[i])) * np.dot(W.T[i],deltaks) for i in range(len(a))]
-	
-	return deltajs
+
+	return np.matrix(deltajs)
 
 def myBackprop(x,t,W1,W2):
 
-	y,z,a = ffnn(x,W1,W2)	# y: Network output
-							# z: hidden layer output
-							# a: hidden layer input
-
+	y,z,a = ffnn(x,W1,W2)
+	# y: Network output
+	# z: hidden layer output
+	# a: hidden layer input
 
 	# Compute output Deltas
 	deltak = outputDeltas(t,y)
-	print("Delta k's: " + str(deltak))
 
 	# Computing network Deltas
-	deltaj2 = networkDeltas(deltak,a,W2)
-	print("Delta j's: " + str(deltaj2))
-	
-	
+	deltaj = networkDeltas(deltak,a,W2)
 
+	dEn_dw1 = deltaj.T * np.matrix(np.insert(x,0,1).T)
+	dEn_dw2 = np.matrix(deltak.T) * np.matrix(np.insert(z,0,1).T)
 
+	return [dEn_dw1, dEn_dw2, y]
 # ----- End Function Definitions ----- #
