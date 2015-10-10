fdd91dcfba48b1bc0c9b065b12b16457c137b3a8
olafurjt13 <olafutjt13@ru.is>
2015-10-10 21:39:17 +0000

    The network Deltas are now computed

--- myBackprop.py
+++ myBackprop.py
@@ -4,6 +4,8 @@
 # error function for a two layer nerual network given an 
 # input pattern x and weight matrices W1 and W2
 # 
+# It is assumed that there is only one output variable y
+#
 # Author: Olafur Jon Thoroddsen
 
 
@@ -11,21 +13,39 @@
 
 import numpy as np
 import sys
-from ffnn, sigmoid import ffnn
+from ffnn import ffnn, sigmoid
 
 # ----- End Importing modules ----- #
 
+
+# ----- Begin Function Definitions ----- #
+
+def outputDeltas(t,y):
+	return np.array(y - t)
+
+def networkDeltas(deltaks,ays,W):
+	a = np.array([sigmoid(value) for value in ays])
+	W = np.delete(W,0)
+	deltajs = [sigmoid(a[i]) * (1 - sigmoid(a[i])) * np.dot(W.T[i],deltaks) for i in range(len(a))]
+	
+	return deltajs
+
 def myBackprop(x,t,W1,W2):
-	y,z,a = ffnn(x,W1,W2)
 
-	if (np.size(t) > 1):
-		print("Target vector larger then 1")
-		i = 0
-		deltak = []
-		for ts in t:
-			dektak[i] = y[i] - ts
-			i += 1
-	else:
-		deltak = y - t
-		print("Target vector is a single number")
+	y,z,a = ffnn(x,W1,W2)	# y: Network output
+							# z: hidden layer output
+							# a: hidden layer input
 
+
+	# Compute output Deltas
+	deltak = outputDeltas(t,y)
+	print("Delta k's: " + str(deltak))
+
+	# Computing network Deltas
+	deltaj2 = networkDeltas(deltak,a,W2)
+	print("Delta j's: " + str(deltaj2))
+	
+	
+
+
+# ----- End Function Definitions ----- #
